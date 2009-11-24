fb924916b399d90d8a20354680f8a945bf236e71
Michael Krisher <mike@mikekrisher.com>
2009-11-24 15:34:01 -0500

    documenting

--- lib/harvestthings/application.rb
+++ lib/harvestthings/application.rb
@@ -13,12 +13,18 @@ module HarvestThings
     # include sync mixin
     include Sync
     
+    # initialize - defines a harvest and things object  
+    #
+    # @return [Boolean]
     def initialize
       @harvest = Harvest.new
       @things = Things.new
       init_sync if config_checks?
     end
     
+    # init_sync - kicks off the syncing  
+    #
+    # @return [String]
     def init_sync
       puts "starting sync..."
       things_projects_to_harvest
@@ -27,6 +33,9 @@ module HarvestThings
     
   private
     
+    # config_checks? - makes sure the config credentials are correct  
+    #
+    # @return [Boolean]
     def config_checks?
       begin
         response = @harvest.request '/clients', :get
