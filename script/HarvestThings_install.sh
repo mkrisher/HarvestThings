11afac836ead5fb3491aa4450bd608e23708790e
Barry Hess <barry@bjhess.com>
2009-12-28 14:17:12 -0600

    Add gemcutter.org as source for rubygems in the isntall script.

--- script/HarvestThings_install.sh
+++ script/HarvestThings_install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo "Installing Harvest Things sync tool..."
+gem sources -a http://gemcutter.org
 sudo gem install harvestthings
 echo "Done installing Harvest Things sync tool"
--- script/HarvestThings_sync.sh
+++ script/HarvestThings_sync.sh
@@ -3,6 +3,7 @@
 if [ ! -f ~/.harvestthingsrc ]
 then
   echo "Installing Harvest Things sync tool..."
+  gem sources -a http://gemcutter.org
   sudo gem install harvestthings
   echo "Done installing Harvest Things sync tool"
 fi
